fc1b00fe2fba2e0cbece81aa57d41dfe77a3c30f
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-09-10 10:23:56 +0100

    display login function Relates #8 Co-authored-by: khadija-nur <59174800+khadija-nur@users.noreply.github.com>

--- create-element.js
+++ create-element.js
@@ -1,5 +1,6 @@
 export function h(tag, props, ...children){
     const el = document.createElement(tag)
+
     const elementWithProps = Object.assign(element, props);
     elementWithProps.append(...children)
     return elementWithProps;
--- login.js
+++ login.js
@@ -21,20 +21,52 @@ if (token) {
     });
 }
 
-function loginForm() {
+function displayLogin() {
     return h(
         "form", {
-            id: "loginForm", // correct id??
-            onsubmit: (event) => {
-                event.preventDefault();
-                const email = event.target.elements.email.value;
-                const password = event.target.elements.password.value;
-                login(email, password).then((user) => {
-                    window.localStorage.setItem("user-token", user.access_token);
+        id: "loginForm", // correct id??
+        onsubmit: (event) => {
+            event.preventDefault();
+            const email = event.target.elements.email.value;
+            const password = event.target.elements.password.value;
+            login(email, password).then((user) => {
+                window.localStorage.setItem("user-token", user.access_token);
 
-                    
-                })
-            }
-        }
-    )
+                const message = h("span", {}, `Greetings Earthling ${user.name}!`);
+                welcome.innerHTML = "";
+                welcome.append(message, logout);
+                loginForm.replaceWith(welcome);
+            });
+        },
+    },
+        h("label", { for: "email" }, "Email"),
+        h("input", {
+            id: "email",
+            type: "email",
+            name: "email",
+            placeholder: "Email",
+        }),
+        h("label", { for: "password" }, "Password"),
+        h("input", {
+            id: "password",
+            type: "password",
+            name: "password",
+            placeholder: "Password",
+            "aria-label": "Password",
+        }),
+        h("button", {}, "Log in")
+    );
+}
+
+function LogoutButton() {
+    return h(
+        "button",
+        {
+            onclick: () => {
+                window.localStorage.removeItem("token");
+                welcome.replaceWith(loginForm);
+            },
+        },
+        "Log out"
+    );
 }
